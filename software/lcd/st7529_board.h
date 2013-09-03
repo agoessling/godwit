5a9fb02caa097e8c42088249d8087b5b043b8f31
Andrew Goessling <agoessling@gmail.com>
2013-09-02 22:22:24 -0700

    Added LCD init parameters

--- software/lcd/st7529_board.h
+++ software/lcd/st7529_board.h
@@ -11,6 +11,27 @@
  * These items are required for the ST7529
  * driver and are specific to the application:
  *
+ * ****** SYSTEM PARAMETERS ********
+ * ST7529_FCPU
+ * ST7529_PWRCTRL_BOOSTER
+ * ST7529_PWRCTRL_PARAM0 
+ * ST7529_VOLCTRL_PARAM0 
+ * ST7529_VOLCTRL_PARAM1 
+ * ST7529_DISCTRL_PARAM0 
+ * ST7529_DISCTRL_PARAM1 
+ * ST7529_DISCTRL_PARAM2 
+ * ST7529_COMSCN_PARAM0  
+ * ST7529_DATSDR_PARAM0  
+ * ST7529_DATSDR_PARAM1  
+ * ST7529_DATSDR_PARAM2  
+ * ST7529_LASET_PARAM0   
+ * ST7529_LASET_PARAM1   
+ * ST7529_CASET_PARAM0   
+ * ST7529_CASET_PARAM1   
+ * ST7529_ANASET_PARAM0  
+ * ST7529_ANASET_PARAM1  
+ * ST7529_ANASET_PARAM2  
+ *
  * ********* PIN MAPPING ***********
  * st7529_RST_DIR_IN()
  * st7529_RST_DIR_OUT()
@@ -39,7 +60,32 @@
  * st7529_delay_us(uint32_t)
  */
 
+#include "cycle_counter.h"
 #include "gpio.h"
+
+/***** SYSTEM PARAMETERS ****/
+#define ST7529_FCPU     25000000    // 25MHz
+
+// These are selected based on the 128240D
+// from DisplayTech
+#define ST7529_PWRCTRL_BOOSTER  0x08    // Booster Must Be On First
+#define ST7529_PWRCTRL_PARAM0   0x0B    // Reference, Follower, Booster On
+#define ST7529_VOLCTRL_PARAM0   0x18    // Vop = 14.8V
+#define ST7529_VOLCTRL_PARAM1   0x04    // Vop = 14.8V
+#define ST7529_DISCTRL_PARAM0   0x00    // No Dividing
+#define ST7529_DISCTRL_PARAM1   0x23    // Duty = 1/144
+#define ST7529_DISCTRL_PARAM2   0x00    // Not Inverted
+#define ST7529_COMSCN_PARAM0    0x02    // 79 -> 0, 80 ->159
+#define ST7529_DATSDR_PARAM0    0x04    // Scan in Line Direction
+#define ST7529_DATSDR_PARAM1    0x00    // Normal pixel Order
+#define ST7529_DATSDR_PARAM2    0x02    // 3B3P Mode
+#define ST7529_LASET_PARAM0     0x00    // Start Line = 0
+#define ST7529_LASET_PARAM1     0x9F    // End Line = 159
+#define ST7529_CASET_PARAM0     0x00    // Start Column = 0
+#define ST7529_CASET_PARAM1     0x54    // End Column = 84
+#define ST7529_ANASET_PARAM0    0x01    // OSC = 17.3kHz
+#define ST7529_ANASET_PARAM1    0x00    // Booster = 3kHz
+#define ST7529_ANASET_PARAM2    0x02    // Bias = 1/12
 
 /******* PIN MAPPING ********/
 #define ST7529_RST_PIN      AVR32_PIN_PA02
@@ -56,35 +102,75 @@
 #define ST7529_D6_PIN       AVR32_PIN_PA22
 #define ST7529_D7_PIN       AVR32_PIN_PA00
 
+#define ST7529_D07_PORTA_PINS   ((1<<ST7529_D0_PIN)|(1<<ST7529_D1_PIN)\
+                                |(1<<ST7529_D1_PIN)|(1<<ST7529_D1_PIN)\
+                                |(1<<ST7529_D1_PIN)|(1<<ST7529_D1_PIN)\
+                                |(1<<ST7529_D1_PIN)|(1<<ST7529_D1_PIN)\
+                                &0xFFFFFFFF)
+
+#define ST7529_D07_PORTB_PINS  (((1<<ST7529_D0_PIN)>>5)|((1<<ST7529_D1_PIN)>>5)\
+                                |((1<<ST7529_D1_PIN)>>5)|(1<<ST7529_D1_PIN>>5)\
+                                |((1<<ST7529_D1_PIN)>>5)|(1<<ST7529_D1_PIN>>5)\
+                                |((1<<ST7529_D1_PIN)>>5)|(1<<ST7529_D1_PIN>>5)\
+                                &0xFFFFFFFF)
+
 #define st7529_RST_DIR_IN()     gpio_local_disable_pin_output_driver(\
                                     ST7529_RST_PIN)
 #define st7529_RST_DIR_OUT()    gpio_local_enable_pin_output_driver(\
                                     ST7529_RST_PIN)
 #define st7529_RST_SET()        gpio_local_set_gpio_pin(ST7529_RST_PIN)
 #define st7529_RST_CLR()        gpio_local_clr_gpio_pin(ST7529_RST_PIN)
-#define st7529_CS_DIR_IN()     gpio_local_disable_pin_output_driver(\
+#define st7529_CS_DIR_IN()      gpio_local_disable_pin_output_driver(\
                                     ST7529_CS_PIN)
-#define st7529_CS_DIR_OUT()    gpio_local_enable_pin_output_driver(\
+#define st7529_CS_DIR_OUT()     gpio_local_enable_pin_output_driver(\
                                     ST7529_CS_PIN)
-#define st7529_CS_SET()        gpio_local_set_gpio_pin(ST7529_CS_PIN)
-#define st7529_CS_CLR()        gpio_local_clr_gpio_pin(ST7529_CS_PIN)
-#define st7529_A0_DIR_IN()     gpio_local_disable_pin_output_driver(\
+#define st7529_CS_SET()         gpio_local_set_gpio_pin(ST7529_CS_PIN)
+#define st7529_CS_CLR()         gpio_local_clr_gpio_pin(ST7529_CS_PIN)
+#define st7529_A0_DIR_IN()      gpio_local_disable_pin_output_driver(\
                                     ST7529_A0_PIN)
-#define st7529_A0_DIR_OUT()    gpio_local_enable_pin_output_driver(\
+#define st7529_A0_DIR_OUT()     gpio_local_enable_pin_output_driver(\
                                     ST7529_A0_PIN)
-#define st7529_A0_SET()        gpio_local_set_gpio_pin(ST7529_A0_PIN)
-#define st7529_A0_CLR()        gpio_local_clr_gpio_pin(ST7529_A0_PIN)
-#define st7529_RD_DIR_IN()     gpio_local_disable_pin_output_driver(\
+#define st7529_A0_SET()         gpio_local_set_gpio_pin(ST7529_A0_PIN)
+#define st7529_A0_CLR()         gpio_local_clr_gpio_pin(ST7529_A0_PIN)
+#define st7529_RD_DIR_IN()      gpio_local_disable_pin_output_driver(\
                                     ST7529_RD_PIN)
-#define st7529_RD_DIR_OUT()    gpio_local_enable_pin_output_driver(\
+#define st7529_RD_DIR_OUT()     gpio_local_enable_pin_output_driver(\
                                     ST7529_RD_PIN)
-#define st7529_RD_SET()        gpio_local_set_gpio_pin(ST7529_RD_PIN)
-#define st7529_RD_CLR()        gpio_local_clr_gpio_pin(ST7529_RD_PIN)
-#define st7529_WR_DIR_IN()     gpio_local_disable_pin_output_driver(\
+#define st7529_RD_SET()         gpio_local_set_gpio_pin(ST7529_RD_PIN)
+#define st7529_RD_CLR()         gpio_local_clr_gpio_pin(ST7529_RD_PIN)
+#define st7529_WR_DIR_IN()      gpio_local_disable_pin_output_driver(\
                                     ST7529_WR_PIN)
-#define st7529_WR_DIR_OUT()    gpio_local_enable_pin_output_driver(\
+#define st7529_WR_DIR_OUT()     gpio_local_enable_pin_output_driver(\
                                     ST7529_WR_PIN)
-#define st7529_WR_SET()        gpio_local_set_gpio_pin(ST7529_WR_PIN)
-#define st7529_WR_CLR()        gpio_local_clr_gpio_pin(ST7529_WR_PIN)
+#define st7529_WR_SET()         gpio_local_set_gpio_pin(ST7529_WR_PIN)
+#define st7529_WR_CLR()         gpio_local_clr_gpio_pin(ST7529_WR_PIN)
+
+inline static void st7529_D07_WRITE(uint8_t data){
+    // Clear Old Values
+    AVR32_GPIO_LOCAL.port[0].ovrc = ST7529_D07_PORTA_PINS;
+    AVR32_GPIO_LOCAL.port[1].ovrc = ST7529_D07_PORTB_PINS;
+
+    // Set Value
+    AVR32_GPIO_LOCAL.port[0].ovrs = st7529_pin_map_lut[data].porta;
+    AVR32_GPIO_LOCAL.port[1].ovrs = st7529_pin_map_lut[data].portb;
+}
+
+inline static uint8_t st7529_D07_READ(void){
+    uint8_t data = 0;
+    
+    data |= (gpio_local_get_pin_value(ST7529_D0_PIN)<<0);
+    data |= (gpio_local_get_pin_value(ST7529_D1_PIN)<<1);
+    data |= (gpio_local_get_pin_value(ST7529_D2_PIN)<<2);
+    data |= (gpio_local_get_pin_value(ST7529_D3_PIN)<<3);
+    data |= (gpio_local_get_pin_value(ST7529_D4_PIN)<<4);
+    data |= (gpio_local_get_pin_value(ST7529_D5_PIN)<<5);
+    data |= (gpio_local_get_pin_value(ST7529_D6_PIN)<<6);
+    data |= (gpio_local_get_pin_value(ST7529_D7_PIN)<<7);
+
+    return data;
+}
+
+/********* HELPER FUNCTIONS ***********/
+#define st7529_delay_us(delay)      cpu_delay_us(delay, ST7529_FCPU)
 
 #endif  // _ST7529_BOARD_H_
